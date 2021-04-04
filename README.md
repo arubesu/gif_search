9036dc0eebaf07bd682da44dd0986e85c726d886
Bruno Souza <bruno.arubesu@gmail.com>
2021-04-03 22:21:03 -0300

    config: env variable

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gif_search
 
-A new Flutter project.
+A new Flutter project
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:flutter/material.dart';
+import 'package:gif_search/pages/home_page.dart';
 
-void main() {
-  runApp(MaterialApp());
+Future main() async {
+  await DotEnv.load(fileName: ".env");
+  runApp(MaterialApp(home: HomePage()));
 }
